7322ba7e85a3c995f872e0afba8add22076ab43d
Kasiagit config --global user.name Kasiagit config --global user.name Kasia <kwerner@centrumcyfrowe.pl>
2023-11-17 11:36:22 +0100

    added error handling and adding books

--- my-books-main/script.js
+++ my-books-main/script.js
@@ -5,110 +5,176 @@ fetch('https://openlibrary.org/subjects/javascript.json')
     // Dane z serwera
     .then(data => {
         const apiBooks = Object.values(data);
-        
+
         const books = apiBooks[4].map(apiBook => ({
             name: apiBook.title,
             author: apiBook.authors.map(author => author.name).join(', ')
         }));
-       
-   
-const renderBookList = (booksToRender) => {
-    const $booksList = document.getElementById('books-list');
 
-    $booksList.innerHTML = '';
+        const renderBookList = (booksToRender) => {
+            const $booksList = document.getElementById('books-list');
 
-    booksToRender.forEach(book => {
-        $booksList.innerHTML += `
+            $booksList.innerHTML = '';
+
+            booksToRender.forEach(book => {
+                $booksList.innerHTML += `
             <li>
                 <h3>${book.name}</h3>
                 <h5>${book.author}</h5>
             </li>
         `;
-    });
-    
-}
+            });
 
-
-document.getElementById('search-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-
-    const phrase = formData.get('phrase');
-
-    if (!phrase) {
-        // alert('Fraza nie moze byc pusta!');
-        renderBookList(books);
-        return;
-    }
-
-    const foundBooks = books.filter(book => book.name.toLowerCase().includes(phrase.toLowerCase()));
-
-    renderBookList(foundBooks);
-});
-
-document.getElementById('add-form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-
-    const title = formData.get('title');
-    const author = formData.get('author');
-
-    //books.push(title, author);
-
-    if (!title && !author) {
-        // alert('Fraza nie moze byc pusta!');
-        renderBookList(books);
-        return;
-    }
-});
-
-document.getElementById('sort-form').addEventListener('change', e => {
-    e.preventDefault();
-    
-    const selectElement = document.querySelector('#sorting-options');
-    let output = selectElement.value;  
-    
-   if(output === 'up'){
-    books.sort((a, b) => {
-        const nameA = a.name.toLowerCase(); 
-        const nameB = b.name.toLowerCase(); 
-        if (nameA < nameB) {
-          return -1;
         }
-        if (nameA > nameB) {
-          return 1;
+
+        document.getElementById('search-form').addEventListener('submit', e => {
+            e.preventDefault();
+
+            const formData = new FormData(e.target);
+
+            const phrase = formData.get('phrase');
+
+            if (!phrase) {
+                // alert('Fraza nie moze byc pusta!');
+                renderBookList(books);
+                return;
+            }
+
+            const foundBooks = books.filter(book => book.name.toLowerCase().includes(phrase.toLowerCase()));
+
+            renderBookList(foundBooks);
+            
+        });
+        const $titleErrorMessage = document.getElementById('title-error');
+        const $titleInput = document.getElementById('title');
+
+        const $authorErrorMessage = document.getElementById('author-error');
+        const $authorInput = document.getElementById('author');
+
+        const validateTitleField = (titleValue) => {
+            if (!$titleErrorMessage)  {
+                $titleInput.classList.add('error-border');
+                // Early return <- wyjscie z funkcji 
+                return false;
+            } 
+            
+            if (!titleValue) {
+                $titleErrorMessage.innerText = 'Pole wymagane!';
+                $titleInput.classList.add('error-border');
+                return false;
+            }
+        
+            // Jeśli dane poprawne, usuń stan błędu 
+            $titleInput.classList.remove('error-border');
+            $titleErrorMessage.innerText = '';
+        
+            return true;
         }
-      
-        return 0;
-      });
-      renderBookList(books);
-    }
 
-    if(output === 'down'){
-        books.sort((b, a) => {
-            const nameA = a.name.toLowerCase(); 
-            const nameB = b.name.toLowerCase(); 
-            if (nameA < nameB) {
-              return -1;
+        const validateAuthorField = (authorValue) => {
+            if (!$authorErrorMessage)  {
+                $authorInput.classList.add('error-border');
+                // Early return <- wyjscie z funkcji 
+                return false;
+            } 
+            // Jeśli authorValue jest falsy dodaj odpowiedni tekst i klasę kolorującą ramkę 
+            if (!authorValue) {
+                $authorErrorMessage.innerText = 'Pole wymagane!';
+                $authorInput.classList.add('error-border');
+                return false;
             }
-            if (nameA > nameB) {
-              return 1;
+        
+            // Jeśli dane poprawne, usuń stan błędu 
+            $authorInput.classList.remove('error-border');
+            $authorErrorMessage.innerText = '';
+        
+            return true;
+        }
+        const booksLocal = localStorage.getItem('books') || '[]';
+        const booksArray = JSON.parse(booksLocal);
+
+        document.getElementById('add-form').addEventListener('submit', e => {
+            e.preventDefault();
+
+            const formData = new FormData(e.target);
+
+            const title = formData.get('title');
+            const author = formData.get('author');
+
+            const isTitleValid = validateTitleField(title);
+            const isAuthorValid = validateAuthorField(author)
+
+            if (!isTitleValid) {
+                $titleInput.focus();
             }
-          
-            return 0;
-          });
-          renderBookList(books);
-    }
+            if (!isTitleValid) return;
 
+            if (!isAuthorValid) {
+                $authorInput.focus();
+            }
+            if (!isAuthorValid) return;
+           
+            const saveData = () => {
+                localStorage.setItem('books', JSON.stringify(booksArray))
+            }
 
-});
+            class NewBook {
+                constructor(name, author) {
+                    this.name = name;
+                    this.author = author;
+                }
+            }
+            
+            const newBook = new NewBook(title, author);
 
-renderBookList(books);
+            booksArray.push(newBook);
+            saveData();
+            books.push(...booksArray);
+            renderBookList(books);
 
+        }); 
 
+        document.getElementById('sort-form').addEventListener('change', e => {
+            e.preventDefault();
 
+            const selectElement = document.querySelector('#sorting-options');
+            let output = selectElement.value;
 
-})
-.catch(error => console.error(error));
+            if (output === 'up') {
+                books.sort((a, b) => {
+                    const nameA = a.name.toLowerCase();
+                    const nameB = b.name.toLowerCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+
+                    return 0;
+                });
+                renderBookList(books);
+            }
+
+            if (output === 'down') {
+                books.sort((b, a) => {
+                    const nameA = a.name.toLowerCase();
+                    const nameB = b.name.toLowerCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+
+                    return 0;
+                });
+                renderBookList(books);
+            }
+
+        });
+        books.push(...booksArray);
+        renderBookList(books);
+
+    })
+    .catch(error => console.error(error));
