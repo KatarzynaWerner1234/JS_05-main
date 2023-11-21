4bddf1cfc2139727b7a2d6de407b8884a2e98bb1
Kasiagit config --global user.name Kasiagit config --global user.name Kasia <kwerner@centrumcyfrowe.pl>
2023-11-21 13:29:01 +0100

    sorting function added

--- list-app/js/main.js
+++ list-app/js/main.js
@@ -6,7 +6,8 @@ fetch('https://jsonplaceholder.typicode.com/users')
             name: user.name,
             userName: user.username
         }));
-        let searchPhrase;
+
+        let sortedUsers;
         let sortDirection;
         const showUsers = (usersToShow) => {
             const $usersList = document.getElementById('users-list');
@@ -17,11 +18,47 @@ fetch('https://jsonplaceholder.typicode.com/users')
                 <h2>${user.name}</h2>
                 <h3><span class="grey">nazwa:</span> ${user.userName}</h3>
                 </li>
-                `; 
-                
+                `;   
             });
         }
         
+        const sortingUserName = (sortDirection) => {
+            if (sortDirection === 'up') {
+                sortedUsers = users.sort((a, b) => {
+                    const nameA = a.userName.toLowerCase();
+                    const nameB = b.userName.toLowerCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+
+                    return 0;
+                });
+                showUsers(sortedUsers);
+               // sortDirection = 'up';
+            }
+
+            if (sortDirection === 'down') {
+                sortedUsers = users.sort((b, a) => {
+                    const nameA = a.userName.toLowerCase();
+                    const nameB = b.userName.toLowerCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+
+                    return 0;
+                });
+                showUsers(sortedUsers);
+               // sortDirection = 'down';
+            }
+
+        }
+
         document.getElementById('search-form').addEventListener('submit', e => {
             e.preventDefault();
             const formData = new FormData(e.target);
@@ -34,45 +71,15 @@ fetch('https://jsonplaceholder.typicode.com/users')
             const foundUsers = users.filter(user => user.userName.toLowerCase().includes(findUsername.toLocaleLowerCase()));
             showUsers(foundUsers);
         });
+
+        
+
         document.getElementById('sort-form').addEventListener('change', e => {
             e.preventDefault();
 
             const selectElement = document.querySelector('#sorting-options');
-            let output = selectElement.value;
-
-            if (output === 'up') {
-                users.sort((a, b) => {
-                    const nameA = a.userName.toLowerCase();
-                    const nameB = b.userName.toLowerCase();
-                    if (nameA < nameB) {
-                        return -1;
-                    }
-                    if (nameA > nameB) {
-                        return 1;
-                    }
-
-                    return 0;
-                });
-                showUsers(users);
-                sortDirection = 'up';
-            }
-
-            if (output === 'down') {
-                users.sort((b, a) => {
-                    const nameA = a.userName.toLowerCase();
-                    const nameB = b.userName.toLowerCase();
-                    if (nameA < nameB) {
-                        return -1;
-                    }
-                    if (nameA > nameB) {
-                        return 1;
-                    }
-
-                    return 0;
-                });
-                showUsers(users);
-                sortDirection = 'down';
-            }
+            sortDirection = selectElement.value;
+            sortingUserName(sortDirection);
 
         });
 
